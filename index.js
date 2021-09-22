6829ab08133504d32d795abe94bde52bcf6efc83
alexlcy <alexlocheungyu@gmail.com.hk>
2021-09-22 23:27:49 +0800

    removed the line

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 mongoose.connect(process.env.MONGO_URL)
     .then(console.log("Connected to Mongo"))
